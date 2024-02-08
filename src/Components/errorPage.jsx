c88eae2c0fb426692958c731e8d4ba81dc667814
vanessa <escavanessa@gmail.com>
2024-02-07 21:33:14 -0500

    added error page. need to route it

---
+++ src/Components/errorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const errorPage = () => {
+  return (
+    <>
+    <h1>Oops!</h1>
+    <h2>It seems like weve hit a bit of a snag - I am a junior developer after all.</h2>
+    <h2>I am going to reroute you back to the home page!</h2>
+    </>
+  )
+}
+
+export default errorPage
